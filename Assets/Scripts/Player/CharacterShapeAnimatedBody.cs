46a4be1703928586b89f99a9840138f883e2a8b7
Shineglow <61727978+Shineglow@users.noreply.github.com>
2024-04-23 19:03:16 +0500

    add animation sprites
    Add sprites to create animations.
    Add stupid animations switch system (use animator instead)

--- Assets/Scripts/Player/CharacterShapeAnimatedBody.cs
+++ Assets/Scripts/Player/CharacterShapeAnimatedBody.cs
@@ -1,9 +1,21 @@
+using System;
 using UnityEngine;
 
 namespace Player
 {
-    public class CharacterShapeAnimatedBody : MonoBehaviour
+    public interface IAnimatedObject
     {
-        
+        void PlayAnimation(string animationName);
+    }
+
+    public class CharacterShapeAnimatedBody : MonoBehaviour, IAnimatedObject
+    {
+        [SerializeField]
+        private Animator animator;
+
+        public void PlayAnimation(string animationName)
+        {
+            animator.Play(animationName);
+        }
     }
 }
